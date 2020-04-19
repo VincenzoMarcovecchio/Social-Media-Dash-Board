36856d5e41e6f706b7cacf8efca735b4f0175624
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2020-04-19 20:23:41 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,22 +6,20 @@ const title = document.querySelectorAll(".title");
 const cards = document.querySelectorAll(".card-big");
 const numbers = document.querySelectorAll("strong");
 const smallCards = document.querySelectorAll(".card-small");
+
 headerRigth.addEventListener("click", () => {
   headerRigth.classList.toggle("header-style");
   ball.classList.toggle("ball-style");
   body.classList.toggle("white");
   header.classList.toggle("azurro");
-
   cards.forEach((element) => {
     element.classList.toggle("bg");
   });
-
   title[0].classList.toggle("black");
   title[1].classList.toggle("black");
   numbers.forEach((element) => {
     element.classList.toggle("black");
   });
-
   smallCards.forEach((element) => {
     element.classList.toggle("bg");
   });
